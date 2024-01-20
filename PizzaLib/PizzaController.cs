8645559467df3b91553f3425476966e3d6eb5142
Malihanaz <gmalihanaz@gmail.com>
2024-01-20 13:47:52 +0530

    phase end project

---
+++ PizzaLib/PizzaController.cs
@@ -0,0 +1,20 @@
+﻿namespace YourNamespace.Tests
+{
+    internal class PizzaController : PizzaControllerTests
+    {
+        internal object Details(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object OrderConfirmation(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
